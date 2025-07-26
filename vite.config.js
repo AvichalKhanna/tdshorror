d11f15f965877a6e510545a92db035e11e4ac952
InCue-in <avichalkhanna111@gmail.com>
2025-07-26 12:03:55 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/tdshorror/',
+   base: '/',
   plugins: [react(), tailwindcss()],
   server: {host:"0.0.0.0", port:5173}
 })
